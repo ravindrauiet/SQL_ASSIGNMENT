1aa3ce7061ee780ba3e8a739f71b2cfb60005cf1
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2024-03-23 22:33:46 +0530

    Advanced Question on join

---
+++ advanced_join_question.sql
@@ -0,0 +1,145 @@
+ASSIGNMENT ON CASE 2 :
+
+
+21.) WAQTD DNAME OF THE EMPLOYEES WHOS NAME IS SMITH
+
+SQL> SELECT DNAME
+  2  FROM EMP,DEPT
+  3  WHERE EMP.DEPTNO IN DEPT.DEPTNO AND
+  4  ENAME IN 'SMITH' ;
+
+DNAME
+--------------
+RESEARCH
+
+22.) WAQTD DNAME AND LOC OF THE EMPLOYEE WHOS ENAME IS KING
+
+SQL> SELECT DNAME , LOC 
+  2  FROM EMP,DEPT
+  3  WHERE EMP.DEPTNO IN DEPT.DEPTNO AND
+  4  ENAME IN 'KING' ;
+
+DNAME          LOC
+-------------- -------------
+ACCOUNTING     NEW YORK
+
+23.) WAQTD LOC OF THE EMP WHOS EMPLOYEE NUMBER IS 7902
+
+SQL> SELECT LOC 
+  2  FROM EMP,DEPT
+  3  WHERE EMP.DEPTNO IN DEPT.DEPTNO AND
+  4  EMPNO IN 7902 ;
+
+LOC
+-------------
+DALLAS
+
+24.) WAQTD DNAME AND LOC ALONG WITH DEPTNO OF THE EMPLOYEE WHOS NAME ENDS WITH 'R'.
+
+SQL>  SELECT DNAME , LOC , EMP.DEPTNO
+  2   FROM EMP,DEPT
+  3   WHERE EMP.DEPTNO IN DEPT.DEPTNO AND 
+  4   ENAME LIKE '%R' ;
+
+DNAME          LOC               DEPTNO
+-------------- ------------- ----------
+SALES          CHICAGO               30
+ACCOUNTING     NEW YORK              10
+
+
+---------------   USING NATURAL JOIN ----------------
+
+SQL> SELECT DNAME , LOC , DEPTNO
+  2  FROM EMP NATURAL JOIN DEPT
+  3 WHERE ENAME LIKE '%R'
+
+
+DNAME          LOC               DEPTNO
+-------------- ------------- ----------
+SALES          CHICAGO               30
+ACCOUNTING     NEW YORK              10
+
+
+
+25.) WAQTD DNAME OF THE EMPLOYEE WHOS DESIGNATION IS PRESIDENT
+
+SQL> SELECT DNAME 
+  2  FROM EMP , DEPT
+  3  WHERE EMP.DEPTNO IN DEPT.DEPTNO AND
+  4  JOB IN 'PRESIDENT' ;
+
+DNAME
+--------------
+ACCOUNTING
+
+26.) WAQTD NAMES OF THE EMPLOYEES WORKING IN ACCOUNTING DEPARTMENT
+
+SQL> SELECT ENAME 
+  2  FROM EMP FULL OUTER JOIN DEPT
+  3  ON EMP.DEPTNO IN DEPT.DEPTNO
+  4  WHERE DNAME IN 'ACCOUNTING' ;
+
+ENAME
+----------
+CLARK
+KING
+MILLER
+
+27.) WAQTD ENAME AND SALARIES OF THE EMPLOYEES WHO ARE WORKING IN THE LOCATION CHICAGO
+
+SQL> SELECT ENAME , SAL 
+  2  FROM EMP , DEPT
+  3  WHERE EMP.DEPTNO IN DEPT.DEPTNO AND
+  4  LOC IN 'CHICAGO' ;
+
+ENAME             SAL
+---------- ----------
+ALLEN            1600
+WARD             1250
+MARTIN           1250
+BLAKE            2850
+TURNER           1500
+JAMES             950
+
+28.) WAQTD DETAILS OF THE EMPLOYEES WORKING IN SALES
+
+SQL> SET PAGES 1000 LINES 10000
+
+
+SQL> SELECT * 
+  2  FROM EMP ,DEPT
+  3  WHERE EMP.DEPTNO IN DEPT.DEPTNO AND 
+  4  DNAME IN 'SALES' ;
+
+     EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO     DEPTNO DNAME          LOC
+---------- ---------- --------- ---------- --------- ---------- ---------- ---------- ---------- -------------- -------------
+      7499 ALLEN      SALESMAN        7698 20-FEB-81       1600        300         30         30 SALES          CHICAGO
+      7521 WARD       SALESMAN        7698 22-FEB-81       1250        500         30         30 SALES          CHICAGO
+      7654 MARTIN     SALESMAN        7698 28-SEP-81       1250       1400         30         30 SALES          CHICAGO
+      7698 BLAKE      MANAGER         7839 01-MAY-81       2850                    30         30 SALES          CHICAGO
+      7844 TURNER     SALESMAN        7698 08-SEP-81       1500          0         30         30 SALES          CHICAGO
+      7900 JAMES      CLERK           7698 03-DEC-81        950                    30         30 SALES          CHICAGO
+
+
+29.) WAQTD DETAILS OF THE EMP ALONG WITH ANNUAL SALARY IF EMPLOYEES ARE WORKING IN NEW YORK
+
+SQL>  SELECT EMP.* , SAL*12 ANNUALSAL
+  2   FROM EMP ,DEPT 
+  3   WHERE EMP.DEPTNO IN DEPT.DEPTNO AND
+  4   LOC IN 'NEW YORK' ;
+
+     EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO  ANNUALSAL
+---------- ---------- --------- ---------- --------- ---------- ---------- ---------- ----------
+      7782 CLARK      MANAGER         7839 09-JUN-81       2450                    10      29400
+      7839 KING       PRESIDENT            17-NOV-81       5000                    10      60000
+      7934 MILLER     CLERK           7782 23-JAN-82       1300                    10      15600
+
+
+30.) WAQTD NAMES OF EMPLOYEES WORKING IN OPERATIONS DEPARTMENT
+
+SQL> SELECT ENAME 
+  2  FROM EMP,DEPT
+  3  WHERE EMP.DEPTNO IN DEPT.DEPTNO
+  4  AND DNAME IN 'OPERATIONS' ;
+
+no rows selected
